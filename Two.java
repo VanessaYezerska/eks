3e75eba166f302e90229144b7f3cdc4b267c7eb4
Vanessa Yezerska <vanessa.yezerska05@gmail.com>
2024-05-29 21:23:06 +0300

    =)

--- Two.java
+++ Two.java
@@ -1,14 +1,16 @@
-public class Two extends One{
+public class Two extends One {
     public Two(int a, int b, int c) {
         super(a, b, c);
     }
+
     public void method3() {
         System.out.println("Це метод: " + a);
     }
+
     public void method4() {
         System.out.println("Це метод: " + b);
     }
-    double methodArifm(){
-        return(a + b)/2;
-    }
+//   double methodArifm(){
+//     return(a + b)/2;
+//   }
 }
